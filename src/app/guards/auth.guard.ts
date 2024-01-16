cc058e11dd02d713c9819464673a9f20690827b9
Dvyd <david.llopislaguna@gmail.com>
2024-01-16 22:31:16 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/app/guards/auth.guard.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageHelper } from '@app/helpers/storage';
 import { environment } from '@environments/environment.development';
 
 @Injectable({
@@ -9,7 +10,7 @@ export class AuthGuard {
   constructor(private router: Router) {}
 
   isLoggedIn() {
-    const user = sessionStorage.getItem(environment.storage.user);
+    const user = StorageHelper.getItem(environment.storage.user, true);
 
     if (user) {
       return true;
