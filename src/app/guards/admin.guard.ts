e0ccb0d9b775311f7a80ec513331521c29f07cb8
Dvyd <david.llopislaguna@gmail.com>
2024-01-24 13:06:44 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/app/guards/admin.guard.ts
    	modified:   src/app/guards/auth.guard.ts

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,17 +1,18 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { StorageHelper } from '@app/helpers/storage';
+import { RolesEnum } from '@app/interfaces/enums/roles.enum';
+import { StorageKeyEnum } from '@app/interfaces/enums/storage.enum';
+import { IUserPayload } from '@app/interfaces/user';
+
 /**
  * Guardia que permite el acceso solo a los usuarios con el rol de administrador.
  * Si el usuario no tiene el rol de administrador, se redirige a la página de chat.
  * @returns {boolean} - Indica si el usuario tiene el rol de administrador y puede acceder.
  */
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { StorageHelper } from '@app/helpers/storage';
-import { RolesEnum } from '@app/interfaces/enums/roles.enum';
-import { IUserPayload } from '@app/interfaces/user';
-
 export const adminGuard: CanActivateFn = () => {
   const router = inject(Router);
-  const user = StorageHelper.getItem<IUserPayload>('usuario');
+  const user = StorageHelper.getItem<IUserPayload>(StorageKeyEnum.User);
 
   if (user?.Role === RolesEnum.Admin) {
     return true;
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,17 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { StorageHelper } from '@app/helpers/storage';
+import { StorageKeyEnum } from '@app/interfaces/enums/storage.enum';
+
 /**
  * Función que actúa como guardia de autenticación para las rutas en Angular.
  * Comprueba si el usuario está autenticado antes de permitir el acceso a una ruta.
  * Si el usuario no está autenticado, redirige a la página de inicio de sesión.
  * @returns {boolean} - Devuelve true si el usuario está autenticado y puede acceder a la ruta, de lo contrario devuelve false.
  */
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { StorageHelper } from '@app/helpers/storage';
-import { environment } from '@environments/environment.development';
-
 export const authGuard: CanActivateFn = () => {
   const router = inject(Router);
-  const user = StorageHelper.getItem(environment.storage.user, true);
+  const user = StorageHelper.getItem(StorageKeyEnum.User);
 
   if (user) {
     return true;
