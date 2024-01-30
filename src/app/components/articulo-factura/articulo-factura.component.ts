b354fb9fbe5829ab842b733653f217d68a55f9d4
JUANMUSA <juanantonio.munozs@gmail.com>
2024-01-30 13:57:24 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/app/components/articulo-factura/articulo-factura.component.html
    	modified:   src/app/components/articulo-factura/articulo-factura.component.ts
    	modified:   src/app/components/loader/loader.component.css
    	modified:   src/app/components/loader/loader.component.html
    	modified:   src/app/pages/factura-avanzado/factura-avanzado.component.html
    	modified:   src/app/pages/factura-avanzado/factura-avanzado.component.ts

--- src/app/components/articulo-factura/articulo-factura.component.ts
+++ src/app/components/articulo-factura/articulo-factura.component.ts
@@ -1,4 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { calculateImporteIva } from '@app/helpers/facturas.helper';
+import { IProduct } from '@app/interfaces/factivar';
 
 /**
  * Componente para mostrar un artículo de una factura.
@@ -6,18 +9,36 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-articulo-factura',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './articulo-factura.component.html',
   styleUrl: './articulo-factura.component.css',
 })
 export class ArticuloFacturaComponent {
+
+  article : IProduct = {
+    pUnitario : 0,
+    unidades : 0,
+    bImponible : 0,
+    cuotaIva : 0,
+    descripcion : "",
+    importe : 0,
+    iva : 0
+  };
+
+
+
+  calculaImporte(){
+    this.article.importe = this.article.unidades * this.article.pUnitario;
+  }
   /**
    * Evento que se emite cuando se desea eliminar el artículo.
    */
   @Output() pacos = new EventEmitter<void>();
+  @Output() articulos = new EventEmitter<IProduct>();
 
-  constructor() {}
-
+  emiteArticle(){
+    this.articulos.emit(this.article);
+  }
   /**
    * Método para eliminar el artículo.
    */
--- src/app/pages/factura-avanzado/factura-avanzado.component.ts
+++ src/app/pages/factura-avanzado/factura-avanzado.component.ts
@@ -3,32 +3,49 @@
  */
 import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { ArticuloFacturaComponent } from '../../components/articulo-factura/articulo-factura.component';
+import { calculateImportes } from '@app/helpers/facturas.helper';
+import { FormsModule } from '@angular/forms';
+import { IProduct } from '@app/interfaces/factivar';
 
 @Component({
   selector: 'app-factura-avanzado',
   standalone: true,
-  imports: [ArticuloFacturaComponent],
+  imports: [ArticuloFacturaComponent, FormsModule],
   templateUrl: './factura-avanzado.component.html',
   styleUrl: './factura-avanzado.component.css',
 })
 export class FacturaAvanzadoComponent {
+  listaArticulos: IProduct[] = [];
   /**
    * Referencia al contenedor de vista del componente ArticuloFactura.
    */
   @ViewChild('articuloFactura', { read: ViewContainerRef })
   articuloFactura!: ViewContainerRef;
 
+  ricias = {
+    subTotal: 0,
+    importeTotal: 0,
+  };
   /**
    * Agrega un artículo al componente ArticuloFactura.
    */
-  addArticle() {
-    this.articuloFactura.createComponent(ArticuloFacturaComponent);
+  // addArticle() {
+  //   this.articuloFactura.createComponent(ArticuloFacturaComponent);
+  // }
+
+  addArticulo(item: IProduct){
+    console.log(item);
+    this.listaArticulos.push(item);
+    console.log(this.listaArticulos);
+    this.ricias = calculateImportes(this.listaArticulos);
   }
+
 
   /**
    * Elimina el artículo del componente ArticuloFactura.
    */
   removeArticle() {
     this.articuloFactura.remove();
+    this.ricias = calculateImportes(this.listaArticulos);
   }
 }
